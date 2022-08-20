e366c31e06d86856f65dd1e8e1eb825be1fde323
K.Himeno <6715229+Himenon@users.noreply.github.com>
2022-08-20 09:39:52 +0900

    feat: add v1.22, v1.23, v1.24, v1.25-rc (#8)

--- scripts/config.ts
+++ scripts/config.ts
@@ -1,6 +1,18 @@
 /**
  * @see https://github.com/kubernetes/kubernetes/blob/master/api/openapi-spec/swagger.json
  */
-export const versions = ["v1.22.3", "v1.19.16", "v1.18.20", "v1.17.17", "v1.16.15", "v1.15.12", "v1.14.10"];
+export const versions = [
+  "v1.25.0-rc.1",
+  "v1.24.4",
+  "v1.23.10",
+  "v1.22.13",
+  "v1.22.3",
+  "v1.19.16",
+  "v1.18.20",
+  "v1.17.17",
+  "v1.16.15",
+  "v1.15.12",
+  "v1.14.10",
+];
 export const outputDir = "api-spec";
 export const sourceOutputDir = "src";
