3ded9aa1e15d41ab4dd66266961bb2e23a001b4e
K.Himeno <6715229+Himenon@users.noreply.github.com>
2024-02-04 01:04:32 +0900

    chore: update

---
+++ tsup.config.ts
@@ -0,0 +1,12 @@
+import { defineConfig } from "tsup";
+
+export default defineConfig({
+  entry: ["./src"],
+  minify: false,
+  target: "es2022",
+  format: ["cjs", "esm"],
+  clean: true,
+  dts: true,
+  tsconfig: "./tsconfig.json",
+  sourcemap: false,
+});
